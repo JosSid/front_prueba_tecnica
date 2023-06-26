cce12c1d5e8cbca4f85b052509ba7c61a3233681
Josep Valls <alfredvays@gmail.com>
2023-06-27 00:44:26 +0200

    add .env

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:3002/user",
+  baseURL: process.env.VUE_APP_API_BASE_URL,
 });
 
 client.interceptors.response.use(
